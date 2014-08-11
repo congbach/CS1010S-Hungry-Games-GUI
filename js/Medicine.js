857e5443d6ee92b7893f64a051c665a02237a434
Cong Bach <thanhcong.bachngoc@gmail.com>
2014-08-11 22:11:42 +0800

    Change Medicine to use different icons based on name, instead of sharing the same ‘Medicine’ icon.

--- js/Medicine.js
+++ js/Medicine.js
@@ -1,13 +1,13 @@
 var cs1010s = cs1010s = cs1010s || {};
 
 cs1010s.Medicine = cc.Sprite.extend({
+    name : null,
     foodValue : null,
     medicineValue : null,
-    name : null,
     owner : null,
 
     ctor:function(name, foodValue, medicineValue, owner) {
-        this._super("images/medicine.png");
+        this._super("images/" + name.replace(/[ ]/g, "_").toLowerCase() + ".png");
         this.name = name;
         this.foodValue = foodValue;
         this.medicineValue = medicineValue;
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    var resources = ["images/square.png", "images/medicine.png"];
+    var resources = ["images/square.png", "images/panadol.png", "images/aloe_vera.png"];
 
     cc.game.onStart = function(){
         //load resources
